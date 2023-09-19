26c9eb032125356faef9e91885a3c0fe2b2a84a1
gustavo.quijada <gustavo.quijada@wildlifestudios.com>
2023-09-19 17:50:44 -0300

    drawing the blocks

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//shared
+// shared
 
 const canvas = document.getElementById("squareCanvas");
 const ctx = canvas.getContext("2d");
@@ -24,6 +24,8 @@ window.onkeyup = function (e) {
     keys[keyDict[e.key]] = false;
 };
 
+// character
+
 let characterWidth = 112 ;
 let characterHeight = 16;
 let characterPositionX = (canvas.width / 2 - characterWidth / 2);
@@ -36,7 +38,7 @@ function drawCharacter(left_key_name, right_key_name) {
         width: characterWidth,
         height: characterHeight,
         vx: 8,
-        color: "green",
+        color: "#F78CA2",
         draw() {
             ctx.beginPath();
             ctx.rect(this.x, this.y, this.width, this.height);
@@ -65,6 +67,8 @@ function drawCharacter(left_key_name, right_key_name) {
 
 var character = drawCharacter('K_a', 'K_d');
 
+//ball
+
 function getRandomSpeed(min, max) {
     min = 6;
     max = 12;
@@ -81,7 +85,7 @@ function drawBall(space_key_name) {
         vx: getRandomSpeed(),
         vy: getRandomSpeed(),
         radius: 8,
-        color: "gray",
+        color: "#F9DEC9",
         isPlaying: false,
         draw() {
             ctx.beginPath();
@@ -131,8 +135,45 @@ function canvasCollision() {
 
 var ball = drawBall('K_Space');
 
+// blocks
+
+const blockRows = 10;
+const blockColumns = 13;
+const blockWidth = (canvas.width / (blockColumns + 1.2));
+const blockHeight = (canvas.height / 2) / 10;
+const blockPadding = 2;
+const blockOffsetTop = 2;
+const blockOffsetLeft = 2; 
+
+const blocks = [];
+for (let c = 0; c < blockColumns; c++){
+    blocks[c] = [];
+    for (let r = 0; r < blockColumns; r++){
+        blocks[c][r] = {x: 0, y: 0};
+    }
+};
+
+function drawBlocks(){
+    for (let c = 0; c < blockColumns; c++){
+        for (let r = 0; r < blockRows; r++){
+            const blockX = (c * (blockWidth + blockPadding)) + blockOffsetLeft;
+            const blockY = (r * (blockHeight + blockPadding)) + blockOffsetTop;
+            blocks[c][r].x = blockX;
+            blocks[c][r].y = blockY;
+            ctx.beginPath();
+            ctx.rect(blockX, blockY, blockWidth, blockHeight);
+            ctx.fillStyle = "#3D0C11";
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+}
+
+// draw phase
+
 function main() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBlocks();
     character.update();
     character.draw();
     character.constrain();
